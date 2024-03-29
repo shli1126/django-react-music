b1f98fd1651993d29107b2cd0ab4fe67c6645ee6
Shaolong Li <shaolongli26@gmail.com>
2024-03-28 22:33:44 -0700

    add join room api

--- django_react_music/frontend/src/components/App.js
+++ django_react_music/frontend/src/components/App.js
@@ -4,7 +4,7 @@ import HomePage from "./HomePage";
 
 const App = () => {
   return (
-    <div>
+    <div className="center">
       <HomePage />
     </div>
   );
--- django_react_music/frontend/src/components/JoinRoomPage.js
+++ django_react_music/frontend/src/components/JoinRoomPage.js
@@ -1,8 +1,68 @@
+import { TextField, Button, Grid, Typography } from "@material-ui/core";
+import { useState } from "react";
 import React from "react";
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const JoinRoomPage = () => {
+  const [roomCode, setRoomCode] = useState("");
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+
+  const handleTextFieldChange = (e) => {
+    setRoomCode(e.target.value);
+  };
+
+  const roomButtonPressed = () => {
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        code: roomCode,
+      }),
+    };
+    fetch("/api/join-room", requestOptions)
+      .then((response) => {
+        if (response.ok) {
+          navigate("/room/" + roomCode);
+        } else {
+          setError("Room not found");
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
-    <h1>This is the JoinRoomPage</h1>
+    <Grid container spacing={1}>
+      <Grid item xs={12} align="center">
+        <Typography variant="h4" component="h4">
+          Join a Room
+        </Typography>
+      </Grid>
+      <Grid item xs={12} align="center">
+        <TextField
+          error={error !== ""}
+          label="Code"
+          placeholder="Enter a Room Code"
+          value={roomCode}
+          helperText={error}
+          variant="outlined"
+          onChange={handleTextFieldChange}
+        />
+      </Grid>
+      <Grid item xs={12} align="center">
+        <Button variant="contained" color="primary" onClick={roomButtonPressed}>
+          Enter Room
+        </Button>
+      </Grid>
+      <Grid item xs={12} align="center">
+        <Button variant="contained" color="secondary" to="/" component={Link}>
+          Back
+        </Button>
+      </Grid>
+    </Grid>
   );
 };
 
