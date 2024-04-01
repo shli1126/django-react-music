13c227154c4b77430f78ad235aefbfc8f99467df
Shaolong Li <shaolongli26@gmail.com>
2024-03-31 23:37:31 -0700

    add home page insatnt join

--- django_react_music/frontend/src/components/HomePage.js
+++ django_react_music/frontend/src/components/HomePage.js
@@ -1,18 +1,61 @@
-import React from "react";
+import React, { useEffect } from "react";
 import CreateRoomPage from "./CreateRoomPage";
 import JoinRoomPage from "./JoinRoomPage";
 import Room from "./Room";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from "react-router-dom";
-
+import { Grid, Button, ButtonGroup, Typography } from "@material-ui/core";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { Navigate } from "react-router-dom";
+import { useState } from "react";
 const HomePage = () => {
+  const [roomCode, setRoomCode] = useState("");
+
+  useEffect(() => {
+    async function componentDidMount() {
+      fetch("/api/user-in-room")
+        .then((response) => response.json())
+        .then((data) => {
+          setRoomCode(data.code);
+        });
+    }
+    componentDidMount()
+  }, []);
+
+  const renderHomePage = () => {
+    return (
+      <Grid container spacing={3}>
+        <Grid item xs={12} align="center">
+          <Typography variant="h3" compact="h3">
+            House Party
+          </Typography>
+        </Grid>
+        <Grid item xs={12} align="center">
+          <ButtonGroup disableElevation variant="contained" color="primary">
+            <Button color="primary" to="/join" component={Link}>
+              Join a Room
+            </Button>
+            <Button color="secondary" to="/create" component={Link}>
+              Create a Room
+            </Button>
+          </ButtonGroup>
+        </Grid>
+      </Grid>
+    );
+  };
+
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<p>HomePage</p>} />
+        <Route
+          path="/"
+          element={
+            roomCode ? (
+              <Navigate to={`/room/${roomCode}`} replace={true} />
+            ) : (
+              renderHomePage()
+            )
+          }
+        />
         <Route path="/join/" element={<JoinRoomPage />} />
         <Route path="/create/" element={<CreateRoomPage />} />
         <Route path="/room/:roomCode/" element={<Room />} />
